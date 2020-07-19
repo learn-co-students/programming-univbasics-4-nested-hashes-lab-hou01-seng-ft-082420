5a0ba8ca6a6f2cb1336ca73825ec671daf20f045
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-19 20:01:29 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,9 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:grace_hopper]
+  
 end
 
 def alan_kay_is_known_for
@@ -36,7 +38,9 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:alan_kay][:known_for]
+  
 end
 
 def dennis_ritchies_language
@@ -55,7 +59,9 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:dennis_ritchie][:languages][0]
+  
 end
 
 def adding_matz
@@ -68,6 +74,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+ 		    :known_for => "Ruby",
+ 		    :languages => ["LISP","C"]
+ 		  },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -95,7 +105,7 @@ def changing_alan
         :languages => ["COBOL", "FORTRAN"]
       },
       :alan_kay => {
-        :known_for => "Object Orientation",
+        :known_for => "GUI",
         :languages => ["Smalltalk", "LISP"]
       },
       :dennis_ritchie => {
@@ -122,7 +132,7 @@ def adding_to_dennis
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => ["C","Assembly"]
       }
     }
 
